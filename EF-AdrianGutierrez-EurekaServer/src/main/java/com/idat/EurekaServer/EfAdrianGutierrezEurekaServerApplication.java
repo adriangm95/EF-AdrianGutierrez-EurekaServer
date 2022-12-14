28889df4d1af65686cbb01c860f1f04ab2ac9f14
adriangm95 <115050376+adriangm95@users.noreply.github.com>
2022-12-15 00:55:21 +1200

    Add files via upload

---
+++ EF-AdrianGutierrez-EurekaServer/src/main/java/com/idat/EurekaServer/EfAdrianGutierrezEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.idat.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EfAdrianGutierrezEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAdrianGutierrezEurekaServerApplication.class, args);
+	}
+
+}
